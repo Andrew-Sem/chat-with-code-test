ef43ebe2e0d0b810db7d56cadf5813ef5e236d79
Andrey Semenov <101043571+Andrew-Sem@users.noreply.github.com>
2024-08-16 23:54:23 +0300

    bot init

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+import { env } from "./src/env";
+import { Telegraf } from "telegraf";
+
+const bot = new Telegraf(env.BOT_TOKEN);
+
+bot.command("start", (ctx) => ctx.reply("Welcome!"));
+
+bot.launch();
