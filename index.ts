912ac20278b9996358ff709f01cb1634c7b5580a
Andrey Semenov <101043571+Andrew-Sem@users.noreply.github.com>
2024-08-17 12:53:46 +0300

    feat: response with RAG

--- index.ts
+++ index.ts
@@ -3,33 +3,51 @@ import { env } from "./src/env";
 import { Telegraf } from "telegraf";
 import { message } from "telegraf/filters";
 import { RetrieveService } from "./src/RetrieveService";
+import dedent from "ts-dedent";
 
 const bot = new Telegraf(env.BOT_TOKEN);
 
 const llm = new ChatGoogleGenerativeAI({
-  model: "gemini-1.5-pro",
+  model: "gemini-1.5-flash",
   temperature: 0.1,
   maxRetries: 2,
 });
 
-bot.command("start", async (ctx) => {
-  const response = await llm.invoke([["human", "Что больше, 9.11 или 9.9"]]);
-  ctx.reply(response.content.toString());
-});
+const retrieveService = new RetrieveService();
 
 bot.on(message("text"), async (ctx) => {
-  const response = await llm.invoke([["human", ctx.message.text]]);
+  const answer = await retrieveService.getAnswer(ctx.message.text);
+
+  const systemMessage = dedent`
+  You are an AI assistant tasked with answering user questions based on the provided context. Your goal is to provide accurate, relevant, and concise responses.
+  
+  Instructions:
+  1. Carefully analyze the following relevant chunks of information:
+  ${JSON.stringify(
+    answer.map((doc) => doc.pageContent),
+    null,
+    2
+  )}
+  2. Use only the information provided in these chunks to formulate your response. Do not use any external knowledge.
+  3. If the provided information is insufficient to fully answer the user's question, clearly state this and explain what specific information is missing.
+  4. If there are any contradictions in the provided chunks, point them out and explain the discrepancy.
+  5. Cite specific chunks when appropriate by referring to them as "according to the provided information" or similar phrasing.
+  6. Keep your response concise and to the point, unless the user specifically requests detailed explanations.
+  7. If you're unsure about any aspect of the answer, express your uncertainty clearly.
+  8. Do not make up or infer information that is not explicitly stated in the provided chunks.
+
+  Remember, your primary goal is to provide accurate and helpful responses based solely on the given context.
+  `;
+  const response = await llm.invoke([
+    ["system", systemMessage],
+    ["human", `User query: ${ctx.message.text}`],
+  ]);
+
   ctx.reply(response.content.toString());
 });
 
-(async () => {
-  const retrieveService = new RetrieveService();
-
-  //   await retrieveService.addData("животные живут на Марсе");
-  //   await retrieveService.addData("Самый главный аракуоль - это аркадий");
-
-  const answer = await retrieveService.getAnswer("Где живут животные");
-  console.log(answer);
-})();
+bot.command("start", async (ctx) => {
+  ctx.reply("Дарова");
+});
 
 bot.launch();
